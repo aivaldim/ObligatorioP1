fb8b49ff95e9f7fc00659ae3f0643cd90eb0f648
Agus Ivaldi <165524356+aivaldim@users.noreply.github.com>
2025-07-02 21:43:00 -0300

    Agregamos los primeros cambios para almacenar el nombre de usuario

--- Obligatorio_P1/main.cpp
+++ Obligatorio_P1/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <stdio.h>
 
-using namespace std;
+const int MAX = 80;
+const int MAX_HEROES = 5;
+
+typedef char String[MAX];
+typedef String superHeroes[MAX_HEROES];
+typedef enum { DC, MARVEL } Universo;
 
 int main()
 {
-    //Commit inicial
+    String nombreUsuario;
+    Universo universo;
+    superHeroes nombreHeroes;
+
+    printf("Ingrese su nombre: ");
+    int i = 0;
+    char ingreso;
+    do{
+        scanf("%c", &ingreso);
+        while( (i < MAX-1) && (ingreso != '\n') ){
+            nombreUsuario[i] = ingreso;
+            i++;
+            scanf("%c", &ingreso);
+        }
+        nombreUsuario[i] = '\0';
+    }while(nombreUsuario[0] == '\0');
+    i = 0;
+
+    while(nombreUsuario[i] != '\0'){
+        printf("%c", nombreUsuario[i]);
+        i++;
+    }
 }
